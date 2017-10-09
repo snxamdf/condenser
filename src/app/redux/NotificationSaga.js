e899223ab0664f883c9efb351d3c820abf9e8c9b
Benjamin Chodoroff <bc@thermitic.net>
2017-10-09 16:36:43 -0400

    wip

--- src/app/redux/NotificationSaga.js
+++ src/app/redux/NotificationSaga.js
@@ -1,13 +1,42 @@
-/**
- * Placeholder for calls to Yo to get notifications.
- * see https://github.com/steemit/yo/issues/10 for Yo json spec
- */
+import { call, put, take, fork, race } from 'redux-saga/effects';
 
-import {takeLatest, takeEvery} from 'redux-saga';
-import {call, put, select, fork} from 'redux-saga/effects';
-import {loadFollows, fetchFollowCount} from 'app/redux/FollowSaga';
-import {getContent} from 'app/redux/SagaShared';
-import GlobalReducer from './GlobalReducer';
-import constants from './constants';
-import {fromJS, Map} from 'immutable'
-import {api} from 'steem';
+function delay(millis) {
+    const promise = new Promise(resolve => {
+        setTimeout(() => resolve(true), millis);
+    });
+    return promise;
+}
+
+function* pollNotifications() {
+    try {
+        yield call(delay, 5000);
+        yield put({
+            type: 'notification/APPEND_SOME_REQUEST',
+            since: 123, // todo: store timestamp in state & use it here -- action should update timestamp when txn is complete
+        });
+    } catch (error) {
+        // cancellation error -- can handle this if you wish
+        return;
+    }
+}
+
+// Wait for successful response, then fire another request
+// Cancel polling if user logs out
+function* watchPollData() {
+    while (true) {
+        yield take([
+            'notification/RECEIVE_ALL', // hang out til we get our first batch of notifs...
+            'notification/APPEND_SOME', // or after one of the polls are done
+        ]);
+        yield race([
+            call(pollNotifications), // and then queue up
+            take('user/LOGOUT'), // or quit if they log out
+        ]);
+    }
+}
+
+export default function* saga() {
+    yield [
+        fork(watchPollData),
+    ];
+}
